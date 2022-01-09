643000f0c6ddd21f6e7dc72a5862226f7844b233
ReshmaSabareesh <reshmaedukat@gmail.com>
2022-01-09 23:34:46 +0530

    change

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ app.post("/send_email",(req,res)=>{
 
 
 
-app.listen(5555,function(){
-    console.log("server is ready at 5555");
-});
-
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
