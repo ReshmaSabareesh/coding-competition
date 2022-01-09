fd5e000349c57912a3a078f518d8f7a42dbe72cc
ReshmaSabareesh <reshmaedukat@gmail.com>
2022-01-09 20:35:29 +0530

    commit on 'master'

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var bodyParser = require('body-parser')
+var cors = require('cors');
+const nodemailer = require('nodemailer');
+
+var app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/',function(req,res){
+    console.log(req);
+    res.send("Welcome to Coding Competition #2 by Reshma E R,Norka FSDb1");
+});
+
+
+app.get("/home",(req,res)=>
+{
+    res.sendFile("./public/home");
+});
+app.post("/send_email",(req,res)=>{
+        const email = req.body.email;
+        const message = {
+            from: 'reshmaedukat@gmail.com', // Sender address
+            to: email,         // List of recipients
+            subject: 'Hello How r u??', // Subject line
+            text: 'simple code challenge!' // Plain text body
+        };
+        let transport = nodemailer.createTransport({
+            service:'gmail',
+            auth: {
+               user: 'reshmasabari87@gmail.com',
+               pass: 'Reshma@1992'
+            }
+        });
+        
+        transport.sendMail(message, function(err, info) {
+            if (err) {
+              console.log(err)
+            } else {
+              console.log(info);
+              return res.send("success");
+            }
+        });
+    
+})
+
+
+
+app.listen(5555,function(){
+    console.log("server is ready at 5555");
+});
+
